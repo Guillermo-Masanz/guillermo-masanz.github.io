4fed7ce8e74b7277123fa0c4a174dc49810bf147
Guillermo-Masanz <ggarciagam1@educacion.navarra.es>
2024-02-14 00:28:14 +0100

    Slider de la página de XBOX

--- js/xbox.js
+++ js/xbox.js
@@ -1,47 +1,29 @@
-const carousel = document.querySelector('.carousel-imagenes');
-const imagenes = Array.from(carousel.children);
-const siguiente = document.querySelector('.forward');
-const antes = document.querySelector('.backward');
-const ancho = imagenes.
+window.onload = carousel();
 
-console.log(imagenes);
+function carousel() {
+    let slider = document.querySelector(".slider");
+    let forward = document.querySelector(".fa-forward");
+    let backward = document.querySelector(".fa-backward");
+    let index = 0;
+    const totalImages = slider.children.length;
 
-// function carrousel(){
-//     const slide = document.querySelectorAll('.slide-image *');
-//     const ancho = slide[0].getBoundingClientRect().width;
-//     console.log(slide);
-//     slide.forEach((slide, index) => {
-//         slide.style.left = ancho * index + 'px';
-//     });
-// };
+    forward.addEventListener("click", () => {
+        index = (index + 1) % totalImages;
+        moveSlider();
+    });
 
-// carrousel();
+    backward.addEventListener("click", () => {
+        index = (index - 1 + totalImages) % totalImages;
+        moveSlider();
+    });
 
+    function moveSlider() {
+        let percentage = index * -100;
+        slider.style.transform = "translateX(" + percentage + "%)";
+    }
 
-// window.onload = function () { 
-//     let slides = document.querySelectorAll('.carousel-item'); 
-  
-//     function addActive(slide) { 
-//         slide.classList.add('active'); 
-//     } 
-  
-//     function removeActive(slide) { 
-//         slide.classList.remove('active'); 
-//     } 
-  
-//     addActive(slides[0]); 
-//     setInterval(function () { 
-//         for (let i = 0; i < slides.length; i++) { 
-//             if (i + 1 === slides.length) { 
-//                 addActive(slides[0]); 
-//                 setTimeout(removeActive, 10000, slides[i]); 
-//                 break; 
-//             } 
-//             if (slides[i].classList.contains('active')) { 
-//                 setTimeout(removeActive, 10000, slides[i]); 
-//                 addActive(slides[i + 1]); 
-//                 break; 
-//             } 
-//         } 
-//     }, 5000); 
-// };
+    setInterval(() => {
+        index = (index + 1) % totalImages;
+        moveSlider();
+    }, 6000);
+}
