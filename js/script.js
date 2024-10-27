44066cc797fd6e1dc9fbbd2a47b61eea76ab875a
Guillermo-Masanz <ggarciagam1@educacion.navarra.es>
2024-10-27 20:32:31 +0100

    bnaba

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const clientId = 'b482b50162c44b21a0a0226f4091fd5b';
-const redirectUri = 'https://guillermo-masanz.github.io/callback.html';
+const redirectUri = 'https://guillermo-masanz.github.io/html/callback.html';
 const scopes = [
     'playlist-read-private',
     'playlist-modify-private',
